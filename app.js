78760594f6434dc454c26d4b9438e8321bb1b5d1
Ramkrish26 <ramakrishnan2629@gmail.com>
2021-10-04 19:10:28 +0530

    Launching react app

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './app.css'
+
+const app = () => (
+    <div className='app'>
+        <h1>Hi</h1>
+    </div>
+);
+
+export default app
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import app from './app.js'
+
+ReactDOM.render(<app/>,document.getElementById('root'))
